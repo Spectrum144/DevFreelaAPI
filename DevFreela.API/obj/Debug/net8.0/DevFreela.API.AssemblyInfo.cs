e8cfd341a8ee4c7612be9be69611b2a0191050d3
Gabriel Uchoa <75533762+Spectrum144@users.noreply.github.com>
2025-06-25 22:58:14 -0300

    Aplicando Migration na nova estrutura

--- DevFreela.API/obj/Debug/net8.0/DevFreela.API.AssemblyInfo.cs
+++ DevFreela.API/obj/Debug/net8.0/DevFreela.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevFreela.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac0b164931db6c03a799c4ab99af25a675e00b31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32e2a0e224c1d0f5a86d8d9a3708b02cea52019b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevFreela.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevFreela.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
